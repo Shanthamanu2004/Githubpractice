a0fe48ef62266b8ca94b26fb30e08614d8f8c544
shant <shant@shantha>
2025-12-03 17:31:45 +0530

    my first commit

---
+++ ProjectForMavenJenkinsProject/src/test/java/Package1/Class2test.java
@@ -0,0 +1,14 @@
+package Package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class2test {
+	
+	@Test
+	private void class1() {
+		Reporter.log(">>>>>>>>>>>>>hey meghana >>>>>>>>>>>>>>",true);
+
+	}
+
+}
